761fea400300f74f977d2432c43085c4589d1a1f
ColinMasucci <colined14@gmail.com>
2025-04-11 23:55:06 +0200

    adding in auth0 part1

--- FocusGroup/src/main.tsx
+++ FocusGroup/src/main.tsx
@@ -1,10 +1,17 @@
+
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { Auth0ProviderWithHistory } from "./auth/Auth0ProviderWithHistory.jsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Auth0ProviderWithHistory>
+      <App />
+    </Auth0ProviderWithHistory>
+  </React.StrictMode>
+);
